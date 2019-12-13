5276191c5ce878dbe8eeaaeda7f59d273ee795eb
fs714 <fs714@163.com>
2019-12-13 19:56:34 +0800

    workable 1.0 version

--- pymsboot/version.py
+++ pymsboot/version.py
@@ -1,6 +1,6 @@
 project_name = 'pymsboot'
-version_string = '0.0.1'
+version_string = '1.0.0'
 release_time = '2018-07-13'
-version_info = 'initial version'
+version_info = 'workable 1.0 version'
 
 version = project_name + ' ' + version_string + ' ' + release_time + ' ' + version_info
