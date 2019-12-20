d30eadb30a860a1be9484fdc5e2bfeb0c0930f6b
fs714 <fs714@163.com>
2019-12-20 15:54:50 +0000

    Update movie api to use sync and async together

--- pymsboot/api/v1/movie.py
+++ pymsboot/api/v1/movie.py
@@ -12,22 +12,22 @@ bp_movie = Blueprint('movie', __name__)
 @bp_movie.route('/movie', methods=['GET'])
 def get_all_movie():
     LOG.info('Get all movie')
-    movies = get_engine_rpc_client().get_all_movie(g.req_ctx)
+    movies = g.dbapi.get_all_movies()
     LOG.info(movies)
     return jsonify({
         'msg': 'success',
-        'data': movies
+        'data': [m.as_dict() for m in movies]
     }), 200
 
 
 @bp_movie.route('/movie/<uuid>', methods=['GET'])
 def get_movie(uuid):
     LOG.info('Get movie {}'.format(uuid))
-    movie = get_engine_rpc_client().get_movie(g.req_ctx, uuid)
+    movie = g.dbapi.get_movie_by_uuid(uuid)
     LOG.info(movie)
     return jsonify({
         'msg': 'success',
-        'data': movie
+        'data': movie.as_dict()
     }), 200
 
 
@@ -37,12 +37,11 @@ def create_movie():
     LOG.info('create movie with ', content)
     content['uuid'] = uuidutils.generate_uuid()
     get_engine_rpc_client().create_movie(g.req_ctx, content_json=content)
+
     return jsonify({
-        'msg': 'success',
-        'data': {
-            'uuid': content['uuid']
-        }
-    })
+        'msg': 'async',
+        'data': ''
+    }), 200
 
 
 @bp_movie.route('/movie/<uuid>', methods=['PUT'])
@@ -50,10 +49,11 @@ def update_movie(uuid):
     content = request.json
     LOG.info('update movie {} with {}'.format(uuid, content))
     get_engine_rpc_client().update_movie(g.req_ctx, uuid=uuid, content_json=content)
+
     return jsonify({
-        'msg': 'success',
+        'msg': 'async',
         'data': ''
-    })
+    }), 200
 
 
 @bp_movie.route('/movie/<uuid>', methods=['DELETE'])
@@ -61,6 +61,6 @@ def delete_movie(uuid):
     LOG.info('delete movie {}'.format(uuid))
     get_engine_rpc_client().delete_movie(g.req_ctx, uuid=uuid)
     return jsonify({
-        'msg': 'success',
+        'msg': 'async',
         'data': ''
-    })
+    }), 200
